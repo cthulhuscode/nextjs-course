b0b333e1e1ede6a2b1df4ca1edb5adf61aa76ad8
Enrique Ruvalcaba <jordison352@gmail.com>
2023-05-27 18:31:16 -0600

    Add path alias to import components

--- playground/components/Layout/Layout.tsx
+++ playground/components/Layout/Layout.tsx
@@ -1,18 +1,17 @@
-import React, { ReactNode } from 'react';
-import { Navbar } from '../Navbar/Navbar';
+import React, { ReactNode } from "react";
+import { Navbar } from "@components/Navbar/Navbar";
 
 type Props = {
   children: ReactNode;
 };
 
-export const Layout: React.FC<Props> = ({children}) => {
-  return (    
+export const Layout: React.FC<Props> = ({ children }) => {
+  return (
     <>
       <Navbar />
       {children}
       <br />
       <footer>Footer</footer>
     </>
-  )
-}
-
+  );
+};
