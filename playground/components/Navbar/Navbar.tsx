223df7bd0907d98f521d7423eeec855af249228e
Enrique Ruvalcaba <jordison352@gmail.com>
2023-05-28 13:47:56 -0600

    Add CSS as module

--- playground/components/Navbar/Navbar.tsx
+++ playground/components/Navbar/Navbar.tsx
@@ -1,14 +1,21 @@
-import React from 'react'
+import React from "react";
 import Link from "next/link";
+import styles from "./navbar.module.css";
 
 export const Navbar = () => {
   return (
-    <nav>
-        <ul>
-            <Link href='/'>Home</Link>
-            <Link href='/about'>About</Link>
-            <Link href='/products'>Products</Link>
-        </ul>
+    <nav className={styles.nav}>
+      <ul className={styles.nav__list}>
+        <Link className={styles.list__option} href="/">
+          Home
+        </Link>
+        <Link className={styles.list__option} href="/about">
+          About
+        </Link>
+        <Link className={styles.list__option} href="/products">
+          Products
+        </Link>
+      </ul>
     </nav>
-  )
-}
+  );
+};
