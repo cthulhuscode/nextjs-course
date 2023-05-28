af6f4a7130446bf586b6fa5ed7135372c3645b00
Enrique Ruvalcaba <jordison352@gmail.com>
2023-05-27 18:17:33 -0600

    Modified default document

---
+++ playground/pages/_document.tsx
@@ -0,0 +1,18 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+ 
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        {/* Favicon */}
+        {/* Webfont */}
+        {/* Stylesheet */}
+        {/* Script/JS */}        
+      </Head>
+      <body className='root'>        
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
