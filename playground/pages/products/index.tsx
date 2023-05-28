f0a8d66ff0b3dc6574834cb028af944eac731b1f
Enrique Ruvalcaba <jordison352@gmail.com>
2023-05-28 13:48:38 -0600

    Add CSS as CSS-in-JS

--- playground/pages/products/index.tsx
+++ playground/pages/products/index.tsx
@@ -1,25 +1,39 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 const Products = () => {
   const [products, setProducts] = useState<TProduct[]>([]);
 
-  const getEntries = () => fetch("api/avocados/")
-    .then(res => res.json())
-    .then(res => setProducts(res.data))
-    .catch(error => console.log(error));
+  const getEntries = () =>
+    fetch("api/avocados/")
+      .then((res) => res.json())
+      .then((res) => setProducts(res.data))
+      .catch((error) => console.log(error));
 
   useEffect(() => {
-    getEntries();    
-  } ,[])
+    getEntries();
+  }, []);
 
   return (
-    <div>
+    <div className="products">
       <h1>Products</h1>
       <ul>
-        {products.map(product => <li key={product.id}>{product.name}</li>)}
+        {products.map((product) => (
+          <li key={product.id}>{product.name}</li>
+        ))}
       </ul>
-    </div>
-  )
-}
 
-export default Products
+      <style jsx>{`
+        .products {
+          margin-top: 20px;
+
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          gap: 40px;
+        }
+      `}</style>
+    </div>
+  );
+};
+
+export default Products;
