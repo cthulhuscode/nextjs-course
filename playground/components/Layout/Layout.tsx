fba069954435927c95848df7a7dc462b5911c61f
Enrique Ruvalcaba <jordison352@gmail.com>
2023-06-04 17:54:31 -0600

    feat: Improve layout

--- playground/components/Layout/Layout.tsx
+++ playground/components/Layout/Layout.tsx
@@ -1,17 +1,61 @@
 import React, { ReactNode } from "react";
-import { Navbar } from "@components/Navbar/Navbar";
+import { ShoppingCartOutlined } from "@ant-design/icons";
+import { Layout, Typography, Button } from "antd";
+import Link from "next/link";
+
+import styles from "./layout.module.css";
+import logo from "../../public/images/logo.png";
+import Image from "next/image";
+
+const { Header, Content, Footer } = Layout;
+const { Title } = Typography;
 
 type Props = {
   children: ReactNode;
 };
 
-export const Layout: React.FC<Props> = ({ children }) => {
+const LayoutFC: React.FC<Props> = ({ children }) => {
   return (
     <>
-      <Navbar />
-      {children}
-      <br />
-      <footer>Footer</footer>
+      <Layout className="layout">
+        <Header
+          style={{
+            height: "60px",
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            background: "#fff",
+          }}
+        >
+          <div>
+            <Link href={"/"}>
+              <div className={styles.layout__title}>
+                <Image
+                  src={logo}
+                  alt="Page logo"
+                  width={38}
+                  quality={50}
+                  priority={true}
+                />
+                <Title style={{ margin: 0 }} level={3}>
+                  Avo Store
+                </Title>
+              </div>
+            </Link>
+          </div>
+          <Button type="default" icon={<ShoppingCartOutlined />} size="large">
+            Cart
+          </Button>
+        </Header>
+
+        <Content style={{ padding: "50px" }}>{children}</Content>
+
+        <Footer style={{ textAlign: "center" }}>
+          Ant Design ©2023 Created by Ant UED
+        </Footer>
+      </Layout>
     </>
   );
 };
+
+export default LayoutFC;
