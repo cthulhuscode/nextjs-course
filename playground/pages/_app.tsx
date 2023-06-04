80801c9d96f1ca19a67b49ebf239dab1078e914b
Enrique Ruvalcaba <jordison352@gmail.com>
2023-06-04 17:55:06 -0600

    feat: new layout

--- playground/pages/_app.tsx
+++ playground/pages/_app.tsx
@@ -1,4 +1,5 @@
-import { Layout } from "../components";
+import Layout from "../components/Layout/Layout";
+import "antd/dist/reset.css";
 import "../style.css";
 
 export default function MyApp({ Component, pageProps }) {
