7bfefa6145a97a924ef9b5e037826c295afe9001
Enrique Ruvalcaba <jordison352@gmail.com>
2023-06-04 22:38:19 -0600

    feat: Show product details

--- playground/pages/products/[id].tsx
+++ playground/pages/products/[id].tsx
@@ -1,6 +1,10 @@
-import  Link from "next/link";
 import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
+import { Col, InputNumber, Row, Typography } from "antd";
+import Image from "next/image";
+import { SpaceContext } from "antd/es/space";
+
+const { Title } = Typography;
 
 const ProductItem = () => {
   const [product, setProduct] = useState<TProduct>();
@@ -19,7 +23,27 @@ const ProductItem = () => {
 
   return (
     <div>
-      xd
+      <Row gutter={[35, 0]} justify={"center"}>
+        <Col>
+          <Image
+            src={product?.image!}
+            width={340}
+            height={340}
+            alt={product?.name!}
+            style={{ borderRadius: "8px" }}
+          />
+        </Col>
+        <Col>
+          <Title>{product?.name}</Title>
+          <p>{`$${product?.price}`}</p>
+          <p>{`SKU: ${product?.sku}`}</p>
+
+          <div style={{ display: "flex", alignItems: "center", gap: "10px" }}>
+            <span>Amount: </span>
+            <InputNumber min={1} defaultValue={1} size="large" />
+          </div>
+        </Col>
+      </Row>
     </div>
   );
 };
