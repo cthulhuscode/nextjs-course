c33b9790404ca6f4a3e97392ff8b0e53f23eb1d9
Enrique Ruvalcaba <jordison352@gmail.com>
2023-06-04 22:38:04 -0600

    feat: show ProductCard

---
+++ playground/components/Product/Product.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { useRouter } from "next/navigation";
+import { Card } from "antd";
+import Image from "next/image";
+
+const { Meta } = Card;
+
+interface ProductProps {
+  product: TProduct;
+}
+
+const Product = ({ product }: ProductProps) => {
+  const router = useRouter();
+
+  const handleClick = (id: string) => router.push(`/products/${id}`);
+
+  return (
+    <Card
+      key={product.id}
+      hoverable
+      style={{ width: 340, position: "relative" }}
+      bordered={true}
+      loading={!product ? true : false}
+      cover={
+        <div>
+          <Image
+            alt={product.name}
+            src={product.image}
+            width={340}
+            height={340}
+          />
+        </div>
+      }
+      onClick={() => handleClick(product.id)}
+    >
+      <Meta title={product.name} description={`$${product.price}`} />
+    </Card>
+  );
+};
+
+export default Product;
