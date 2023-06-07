c6d28cc8368a03a7f4a8b2187d9135097c354710
Enrique Ruvalcaba <jordison352@gmail.com>
2023-06-06 23:26:02 -0600

    feat: Add list to show product details

--- playground/pages/products/[id].tsx
+++ playground/pages/products/[id].tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
-import { Col, InputNumber, Row, Space, Typography } from "antd";
+import { Col, InputNumber, List, Row, Space, Typography } from "antd";
 import Image from "next/image";
 import { SpaceContext } from "antd/es/space";
 
@@ -20,6 +20,8 @@ const ProductItem = () => {
   useEffect(() => {
     id && getEntry();
   }, [id]);
+
+  if (!product) return;
 
   return (
     <div>
@@ -45,10 +47,36 @@ const ProductItem = () => {
         </Col>
       </Row>
       <Space />
-      <Row justify={"center"} style={{ gap: "30px" }}>
+      <Row justify={"center"} style={{ gap: "30px", marginTop: "30px" }}>
         <Col span={24} style={{ maxWidth: "655px" }}>
           <Title level={3}>About this avocado</Title>
           <p>{product?.attributes.description}</p>
+        </Col>
+      </Row>
+      <Row justify={"center"}>
+        <Col style={{ width: "200px" }}>
+          <List itemLayout="horizontal">
+            <List.Item>
+              <List.Item.Meta
+                title={<a href="https://ant.design">Shape</a>}
+                description={product.attributes.shape}
+              />
+            </List.Item>
+
+            <List.Item>
+              <List.Item.Meta
+                title={<a href="https://ant.design">Hardiness</a>}
+                description={product.attributes.hardiness}
+              />
+            </List.Item>
+
+            <List.Item>
+              <List.Item.Meta
+                title={<a href="https://ant.design">Taste</a>}
+                description={product.attributes.taste}
+              />
+            </List.Item>
+          </List>
         </Col>
       </Row>
     </div>
