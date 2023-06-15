30b1d312dfc9dae88d6d1645842f0e6ab932894a
cthulhuscode <jordison352@gmail.com>
2023-06-15 14:40:39 -0600

    feat: access to cart context

--- playground/pages/_app.tsx
+++ playground/pages/_app.tsx
@@ -1,11 +1,15 @@
 import Layout from "../components/Layout/Layout";
 import "antd/dist/reset.css";
 import "../style.css";
+import { CartProvider } from "store/Cart/cartContext";
+
 
 export default function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <CartProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </CartProvider>
   );
 }
