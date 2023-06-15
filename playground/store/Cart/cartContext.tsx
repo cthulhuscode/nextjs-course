3f4d656956ed670c7833ea423a5dd93602006120
cthulhuscode <jordison352@gmail.com>
2023-06-15 17:15:46 -0600

    style: New cart reducer

--- playground/store/Cart/cartContext.tsx
+++ playground/store/Cart/cartContext.tsx
@@ -1,16 +1,17 @@
 import { ReactNode, createContext, useContext, useReducer } from "react";
+import { cartReducer } from "./cartReducer";
 
 type CartProviderProps = { children: ReactNode };
 export type Action = { type: string; payload: any | undefined };
 type Dispatch = (action: Action) => void;
 export type State = {
-  items: { product: TProduct; amount: number; price: number }[] | null;
+  items: { product: TProduct; amount: number; price: number }[];
   count: number;
   totalPrice: number;
 };
 
 const initialState: State = {
-  items: null,
+  items: [],
   count: 0,
   totalPrice: 0,
 };
@@ -18,19 +19,6 @@ const initialState: State = {
 const CartStateContext = createContext<
   { state: State; dispatch: Dispatch } | undefined
 >(undefined);
-
-function cartReducer(state: State, action: Action) {
-  switch (action.type) {
-    case "cart/addProduct": {
-      return {
-        ...state
-      };
-    }
-    default: {
-      throw new Error(`Unhandled action type: ${action.type}`);
-    }
-  }
-}
 
 export function CartProvider({ children }: CartProviderProps) {
   const [state, dispatch] = useReducer(cartReducer, initialState);
--- playground/store/Cart/cartReducer.ts
+++ playground/store/Cart/cartReducer.ts
@@ -1,15 +1,52 @@
 import { TAddToCart } from "./actionTypes";
 import { Action, State } from "./cartContext";
 
-export function cartReducer(state: State, action: Action) {
-    switch (action.type) {
-      case TAddToCart: {
-        return {
-          ...state
+export function cartReducer(state: State, action: Action) { 
+
+  switch (action.type) {
+    case TAddToCart: {
+      const { product, amount } = action.payload;      
+      const productExists = state.items?.find((item) => item.product.id === product.id);
+
+      let newState: State = {
+        items: [],
+        count: 0,
+        totalPrice: 0
+      };
+      if(!productExists && state.items.length === 0){
+        const newItem = { product, amount, price: amount * product.price };
+        newState = { ...state, items: [newItem] };
+      }
+      else if (!productExists) {
+        const newItem = { product, amount, price: amount * product.price };
+        newState = { ...state, items: [...state.items, newItem] };
+      } else {
+        const cartWithoutCurrentProduct = state.items?.filter(
+          (item) => item.product.id !== product.id
+        );
+
+        const newAmount = productExists.amount + amount;
+        const itemUpdated = {
+          product,
+          amount: newAmount,
+          price: product * newAmount,
         };
+
+        newState = { ...state, items: [...cartWithoutCurrentProduct, itemUpdated] };
       }
-      default: {
-        throw new Error(`Unhandled action type: ${action.type}`);
-      }
+
+      // Calculate the amount of items in the cart
+      newState.count += amount;
+      // Calculate the cart's total price
+      newState.totalPrice += product.price * amount;
+
+      return {
+        ...newState,
+      };
+    }
+
+    default: {
+      throw new Error(`Unhandled action type: ${action.type}`);
     }
   }
+}
