b413fc93691f54e1a97e932ca2dc8a0076999ff6
cthulhuscode <jordison352@gmail.com>
2023-07-16 21:53:21 -0600

    chore: check metrics

--- playground/pages/_app.tsx
+++ playground/pages/_app.tsx
@@ -1,10 +1,12 @@
+import { useReportWebVitals } from "next/web-vitals";
 import Layout from "../components/Layout/Layout";
 import "antd/dist/reset.css";
 import "../style.css";
 import { CartProvider } from "store/Cart/cartContext";
 
-
 export default function MyApp({ Component, pageProps }) {
+  useReportWebVitals(metric => console.log("reportWebVitals",metric));
+
   return (
     <CartProvider>
       <Layout>
